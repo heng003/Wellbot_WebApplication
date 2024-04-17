040d792ff3413dad5079d1d4f026721973080c0e
XinTian-1101 <lyexintian@gmail.com>
2024-04-17 18:26:39 +0800

    85%

--- client/src/TenantPOV/component/CardRent.jsx
+++ client/src/TenantPOV/component/CardRent.jsx
@@ -4,48 +4,20 @@ import downLoad_Icon from '../component/Rental_Icon/download.png';
 import Comment_Icon from '../component/Rental_Icon/comment.png';
 
 const CardRent = ({ listing }) => {
-<<<<<<< HEAD
-    const { title, locationOwner, duration, isActive } = listing; 
-=======
     const { title, locationOwner, duration, isActive } = listing;
 
->>>>>>> rental_rate/tenant
 
     return (
 
-
-<<<<<<< HEAD
-=======
-
-
->>>>>>> rental_rate/tenant
         <div className="rentalList_card">
 
-
-
-<<<<<<< HEAD
-            <div className={`history-listing ${isActive ? 'active' : 'expired'}`}>
-
-=======
-
-
-
             <div className={`history-listing ${isActive ? 'active' : 'expired'}`}>
 
 
->>>>>>> rental_rate/tenant
                 <div className="rentalHistory-image">
                     <img src={listing.imageUrl} alt="Rental Property" />
                 </div>
 
-<<<<<<< HEAD
-                <div className="rentalHistory-details">
-                    <h2 className="rental_historyTitle">{title}</h2>
-                    <p className="descript_rental">{locationOwner}</p> 
-                    <p className="descript_duration">Duration: {duration}</p>
-                </div>
-
-=======
 
                 <div className="rentalHistory-details">
                     <h2 className="rental_historyTitle">{title}</h2>
@@ -54,7 +26,6 @@ const CardRent = ({ listing }) => {
                 </div>
 
 
->>>>>>> rental_rate/tenant
                 <div className="property-actions">
                     {isActive ? (
                     <button className="view-agreement-btn">Active</button>
@@ -63,15 +34,6 @@ const CardRent = ({ listing }) => {
                     )}
                     </div>
 
-<<<<<<< HEAD
-                <div className="contact-icons">
-                    <img src={downLoad_Icon} alt="Download" />
-                    <img src={Comment_Icon} alt="Message" /> 
-                </div>
-
-            </div>
-
-=======
 
                 <div className="contact-icons">
                     <img src={downLoad_Icon} alt="Download" />
@@ -82,15 +44,10 @@ const CardRent = ({ listing }) => {
             </div>
 
 
->>>>>>> rental_rate/tenant
         </div>
    
     );
 };
 
-<<<<<<< HEAD
-export default CardRent;
-=======
 
 export default CardRent;
->>>>>>> rental_rate/tenant
