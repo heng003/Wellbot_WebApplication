e332454b82ea4ece8a0b7841a9bf4a59c9d03231
XinTian-1101 <lyexintian@gmail.com>
2024-04-22 12:14:24 +0800

    60_complete_new_design

--- client/src/LogIn/LogIn.jsx
+++ client/src/LogIn/LogIn.jsx
@@ -22,46 +22,45 @@ const LogIn = () => {
     }
 
     return(
-        <div id="login">
+
+        <div id="login" className="d-flex">
 
             <img src="Images/authe_logo.png" alt="Logo" width='140' height='140'/>
 
-            <div className="Left_Design">
-                <h2 className="logIn_wlcTxt">Welcome Back To <br></br>RentSpotter !</h2>
-                <p className="brief_txt">Direct Dialogue: Your Bridge-Free<br></br> Connection to Home</p>
-                <img src="Images/signIn_pic.svg" alt="signIn_pic" width='900' height='auto' style={{marginLeft:'-12em', marginTop:'-8.5em'}}/>
-            </div>
+            <div className="content-section">
+                <div className="left-section">
+                    <h2 className="logIn_wlcTxt">Welcome Back To <br/>RentSpotter !</h2>
+                    <p className="brief_txt">Direct Dialogue: Your Bridge-Free<br/> Connection to Home</p>
+                    <img src="Images/signIn_pic.svg" alt="signIn_pic" width='900' height='auto' style={{marginLeft:'-12em', marginTop:'-8em'}}/>
+                </div>
 
-            <div class="container_login text-center">
-            <div class="row">
-                
-                <div class="col rightCol" id="login-form">
-                    <h2 className="loginTitle fs-2 fw-bolder mt-4">Log In</h2>
-                    <div class="form d-flex flex-row align-items-center mb-4">
-                        <FontAwesomeIcon icon={faUser} className="fa-lg me-3 fa-fw" />
-                        <div class="form-outline flex-fill mb-0">
-                            <input type="text" id="username" class="form-control" placeholder="Username" required onChange={(e) => setUsername(e.target.value)}/>
+                <div className="right-section">
+                    <div className="login-form">
+                        <h2 className="loginTitle fs-2 fw-bolder mt-4">Log In</h2>
+
+                        <div className="form align-items-center mb-4">
+                            <FontAwesomeIcon icon={faUser} className="fa-lg me-3 fa-fw"size="2x"/>
+                            <input type="text" id="username" className="form-control" placeholder="Username" required onChange={(e) => setUsername(e.target.value)}/>
                         </div>
-                    </div>
-                    <div class="form d-flex flex-row position-relative align-items-center mb-4">
-                        <FontAwesomeIcon icon={faLock} className="fa-lg me-3 fa-fw"/>
-                        <div class="form-outline flex-fill mb-0 position-relative">
-                            <input value={password} type={visible ? "text" : "password"} id="password" class="form-control" placeholder="Password" onChange={e => setPassword(e.target.value)} required/>  
+
+                        <div className="form align-items-center mb-4">
+                            <FontAwesomeIcon icon={faLock} className="fa-lg me-3 fa-fw"size="2x"/>
+                            <input value={password} type={visible ? "text" : "password"} id="password" className="form-control" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required/>
+                                <div className="eye-icon" onClick={() => setVisible(!visible)}>
+                                    {visible ? <FontAwesomeIcon icon={faEye} className="fa-lg fa-fw"/> : <FontAwesomeIcon icon={faEyeSlash} className="fa-lg fa-fw"/>}
+                                </div>
                         </div>
-                        <div className="eye-icon" onClick={() => setVisible(!visible)}>{ visible ? <FontAwesomeIcon icon={faEye} className="fa-lg me-3 fa-fw"/> : <FontAwesomeIcon icon={faEyeSlash} className="fa-lg me-3 fa-fw"/> }</div>
-                    </div>
-                    <div className="forgotPassword">
-                        <Link className="link" id="forgotpasswwordlink" to="/forgotPassword"><h6 className="forgotPassword">Forgot password?</h6></Link>
-                    </div>
-                    <div id="bottomDetails">
-                        <button id="loginbutton" type="button" onClick={handleLogin}>Log In</button>
-                        <div id="haveAcc">Don't have an account ? <Link className="link" id="signuplink" to="/signIn">SIGN UP</Link></div>
-                    </div>
+
+                <Link to="/forgotPassword" className="forgot-password-link">Forgot password?</Link>
+                <button id="login-button" type="button" onClick={handleLogin}>Log In</button>
+                <p className="account-option">
+                Don't have an account? <Link to="/signIn" className="sign-up-link">SIGN UP</Link>
+                </p>
                 </div>
             </div>
-            </div>
         </div>
-    );
+  </div>
+);
 }
 
 export default LogIn;
