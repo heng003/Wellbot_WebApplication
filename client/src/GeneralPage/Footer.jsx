6a3852fb484dfa6b7033cf99ccc05e24c8c65568
celinezyyy <celineong1016@gmail.com>
2024-04-26 12:26:59 +0800

    merge

--- client/src/GeneralPage/Footer.jsx
+++ client/src/GeneralPage/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
     return(
