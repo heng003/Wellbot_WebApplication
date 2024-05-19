bac2a26027f4c1079a3cf22104ff2116362015a7
XiaoXuan <hengxiaoxuan2003@gmail.com>
2024-05-20 00:07:10 +0800

    get user profile by userid

--- server/tenantApplication/middlwares/userIdMiddleware.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('express-jwt');
-
-const authenticate = jwt({
-  secret: process.env.JWT_SECRET,
-  algorithms: ['HS256'],
-  userProperty: 'auth', // The property on req object where the payload will be attached
-});
-
-module.exports = authenticate;
