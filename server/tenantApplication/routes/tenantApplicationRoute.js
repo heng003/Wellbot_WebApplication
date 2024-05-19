dc8a6b39cfa2bc8ef811f3cb0933ade7be0c034f
XiaoXuan <hengxiaoxuan2003@gmail.com>
2024-05-20 00:07:10 +0800

    get user profile by userid

--- server/tenantApplication/routes/tenantApplicationRoute.js
+++ server/tenantApplication/routes/tenantApplicationRoute.js
@@ -3,29 +3,17 @@ const router = express.Router();
 const authenticate = require("../middlwares/userIdMiddleware");
 const tenantController = require("../controllers/tenantController");
 
-<<<<<<< HEAD
-router.get("/", tenantController.getAllProperties);
-router.get("/condo", tenantController.getAllCondoProperties);
-router.get("/commercial", tenantController.getAllComercialProperties);
-router.get("/ViewProperty/:propertyId", tenantController.getOneProperty);
-router.get("/tenantViewProperty/:propertyId", tenantController.getOneProperty);
-router.get("/tenantApplyForm/:userId", tenantController.getUserProfile);
+router.get("/", authenticate, tenantController.getAllProperties);
 router.get(
-  "/tenantApplyForm/:userId/:propertyId",
-  tenantController.checkApplicationExists
+  "/ViewProperty/:propertyId",
+  authenticate,
+  tenantController.getOneProperty
 );
-router.post("/tenantApplyForm", tenantController.createApplication);
-router.get("/tenantApplication/:userId", tenantController.getApplications);
-router.get("/landlord/:landlordId", tenantController.getLandlord);
-router.get("/landlordReview/:landlordId", tenantController.getLandlordReview);
 router.get(
-  "/tenantToLease/:applicationId",
-  tenantController.getLeaseByApplicationId
+  "/tenantViewProperty/:propertyId",
+  authenticate,
+  tenantController.getOneProperty
 );
-=======
-router.get('/', tenantController.getAllProperties);
-router.get('/ViewProperty/:propertyId', tenantController.getOneProperty);
-router.get('/tenantViewProperty/:propertyId', tenantController.getOneProperty);
->>>>>>> 314ff59 (jwt installed)
+router.get("/tenantApplyForm", authenticate, tenantController.getUserProfile);
 
 module.exports = router;
