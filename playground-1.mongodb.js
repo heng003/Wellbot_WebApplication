44309d603042c38933b900b6fbab72b0640cfcac
XinTian-1101 <lyexintian@gmail.com>
2024-05-21 16:37:30 +0800

    Done_Landlord_POV

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -1,10 +1,10 @@
+use("RentSpotter");
 
-use('RentSpotter');
-
-db.getCollection('reviewlandlords').insertOne({
-  landlordId: new ObjectId('664675d28718fb6fe7e7c251'),
-  tenantId: new ObjectId('663c7b00472233659b0e1a69'),
+db.getCollection("reviewlandlords").insertOne({
+  landlordId: new ObjectId("664675d28718fb6fe7e7c251"),
+  tenantId: new ObjectId("663c7b00472233659b0e1a69"),
   landlordRating: 4,
-  commentLandlord: "Exceptional landlord! Always responsive to maintenance requests, kept the property well-maintained, and communicated effectively. Demonstrated professionalism and respect. I highly recommend this landlord.",
-  commentDate: new Date(Date.now())
+  commentLandlord:
+    "Exceptional landlord! Always responsive to maintenance requests, kept the property well-maintained, and communicated effectively. Demonstrated professionalism and respect. I highly recommend this landlord.",
+  commentDate: new Date(Date.now()),
 });
