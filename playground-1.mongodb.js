e87fcd5835dc3748dbb8c915267cd13912fdde55
XinTian-1101 <lyexintian@gmail.com>
2024-05-21 16:37:30 +0800

    Done_Landlord_POV

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -1,12 +1,8 @@
 use("RentSpotter");
 
-db.getCollection("leases").insertOne({
-  tenantId: ObjectId("663c7b00472233659b0e1a69"),
-  propertyId: ObjectId("664770d50a1b773d78a1f9b7"),
-  leaseStatus: "Under Review By Tenant",
-  effectiveDateStart: null,
-  effectiveDateEnd: null,
-  PDF: null,
+db.getCollection("applications").insertOne({
+  tenantId: new ObjectId("664efa491ff11b5274cfe440"),
+  propertyId: new ObjectId("664770d50a1b773d78a1f9b7"),
+  applicationStatus: "Pending",
+  __v: 0,
 });
-
-db.getCollection("leases").find().pretty();
