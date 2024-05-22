c38f643746cca7109aefa15f8205aa3859886a81
“Joshua <jlck1@outlook.com>
2024-05-22 15:28:07 +0800

    Completed basic APIs and routes

--- server/models/applicationModel.js
+++ server/models/applicationModel.js
@@ -3,6 +3,15 @@ const { Schema } = mongoose;
 
 const applicationSchema = new Schema({
   tenantId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  landlordId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  propertyId: { type: Schema.Types.ObjectId, ref: "Property", required: true },
+  applicationStatus: {
+    type: String,
+    enum: ["Pending", "Approved", "Rejected", "Active"],
+    default: "Pending",
+    required: true,
+  },
+  tenantId: { type: Schema.Types.ObjectId, ref: "User", required: true },
   propertyId: { type: Schema.Types.ObjectId, ref: "Property", required: true },
   applicationStatus: {
     type: String,
