a6c09efa0af4d0773084053612d6528c100b4326
XinTian-1101 <lyexintian@gmail.com>
2024-06-05 00:01:08 +0800

    update_all_card

--- client/src/component/CardGeneral.jsx
+++ client/src/component/CardGeneral.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import '../GeneralPage/home.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import { useNavigate } from "react-router-dom";
+import { CardBody, CardContainer, CardItem } from "../LandlordPOV/components/ui/3d-card";
 
 const CardGeneral = ({ propertyId, imgSrc, cardTitle, propertyTitle, propertyAdd, roomDetails }) => {
     const roomImgSrc = ["Images/bedroom.png", "Images/bathroom.png", "Images/sqrt.png"];
@@ -13,45 +14,43 @@ const CardGeneral = ({ propertyId, imgSrc, cardTitle, propertyTitle, propertyAdd
         navigate(`/ViewProperty/${propertyId}`);
     }
 
-  return (
-    <div className="card h-100" onClick={handleViewProperty}>
-      <div>
-        <img src={imgSrc} className="card-img-top" alt="propertyImage" />
-      </div>
-      <div className="card-body d-flex flex-column justify-content-between">
-        <div className="card-desription-container">
-          <h4 className="card-title1">{cardTitle}</h4>
-          <h6 className="card-title2">{propertyTitle}</h6>
-          <p className="card-text">{propertyAdd}</p>
-        </div>
+    return (
+      <CardContainer className="card_Container">
+          <CardBody className="card h-100" onClick={handleViewProperty}>
+          <CardItem onClick={handleViewProperty} translateZ="100">
+              <img src={imgSrc} className="card-img-top" alt="propertyImage"/>
+              </CardItem>
+              <CardItem onClick={handleViewProperty} translateZ="60" className="card-body d-flex flex-column justify-content-between">
+                  <CardItem onClick={handleViewProperty} className="card-desription-container">
+                      <h4 className="card-title1">{cardTitle}</h4>
+                      <h6 className="card-title2">{propertyTitle}</h6>
+                      <p className="card-text">{propertyAdd}</p>
+                  </CardItem>
 
-        <ul className="roomIconsGroup">
-          {roomDetails.map((detail, index) => (
-            <li key={index} className="roomDetailsOneIconGroup">
-              <img
-                src={roomImgSrc[index]}
-                alt="details"
-                width="35"
-                height="35"
-                style={{ marginRight: "0.5em" }}
-              />{" "}
-              {detail}
-            </li>
-          ))}
-        </ul>
+              <ul className="roomIconsGroup">
+              {roomDetails.map((detail, index) => (
+                  <li key={index} className="roomDetailsOneIconGroup">
+                  <img
+                      src={roomImgSrc[index]}
+                      alt="details"
+                      width="35"
+                      height="35"
+                      style={{ marginRight: "0.5em" }}
+                  />{" "}
+                  {detail}
+                  </li>
+              ))}
+              </ul>
 
-        <div className="viewButton">
-          <button
-            className="View-Button"
-            type="button"
-            onClick={handleViewProperty}
-          >
-            View
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-};
+              <CardItem onClick={handleViewProperty} className="viewButton" translateZ="60">
+                  <button className="View-Button" type="button">View</button>
+              </CardItem>
+              
+          </CardItem>
+      </CardBody>
+      </CardContainer>
+  )
+}
+
 
 export default CardGeneral;
